129fd32e5c8c7ae0f09a52bc0c4ce76536415a52
Celina Shen <celinashen@Celinas-MacBook-Air.local>
2024-12-04 14:38:11 -0800

    Adding all my class files and labs to the repository

---
+++ Class 4/shen_class04.R
@@ -0,0 +1,8 @@
+#My first Rscript
+x <- 1:50
+plot(x)
+
+plot(x, sin(x))
+plot(x, sin(x), typ = "l")
+plot(x, sin(x), typ = "l", col = "lightblue")
+plot(x, sin(x), typ = "l", col = "magenta", lwd = 3)
---
+++ Find a gene/find a gene.R
@@ -0,0 +1,2 @@
+library(bio3d)
+read.fasta(muscle-I20241202-013848-0634-46176942-p1m.aln-clustalw.fst)
